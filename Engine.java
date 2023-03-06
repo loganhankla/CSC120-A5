58321bb94c455de804142870093e4f73f821b040
Logan Hankla <chankla@smith.edu>
2023-03-06 15:24:50 -0500

    built constructor

--- Engine.java
+++ Engine.java
@@ -1,11 +1,41 @@
 public class Engine {
+
+    private FuelType f;
+
+    private double currentFuelLevel;
+
+    private double maxFuelLevel;
 // fuel type
 // capcity
 // current level
 
-//constructor
+//accessors
+//getFuelLevel()? others? getFuelType
 
-//refuel()
+//constructor
+    public Engine(FuelType f, double fuel){
+        this.f = f;
+        this.currentFuelLevel = fuel;
+        this.maxFuelLevel= fuel;
+    }
+
+//refuel
+    public void refuel(){
+        this.currentFuelLevel = this.maxFuelLevel;
+    }
 
 //go() to modify fuel level
+    public void go(){
+
+    }
+
+
+
+//main function just for testing
+public static void main() {
+    Engine myEngine = Engine(FuelType.ELECTRIC, 100.0);
+
+
+}
+
 }
