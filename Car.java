ddb02fdd40f36d00902b4b1cf0faa966a9ec433d
Logan Hankla <chankla@smith.edu>
2023-03-08 21:09:01 -0500

    added Javadoc to Car class

--- Car.java
+++ Car.java
@@ -1,10 +1,17 @@
+/**
+ * @author Logan Hankla
+ * The Car class creates cars, stores information about capacity and passengers.
+ */
+
 import java.util.ArrayList;
 
 public class Car {
     private ArrayList<Passenger> passengersOnboard; 
     private int maxCapacity;
 
-    //constructor
+    /** Car constructor helps create cars
+     * @param max The initial value for the maximum capacity of a car.
+     */
     public Car(int max){
         //take in initial value for max capacity
         this.maxCapacity = max;
@@ -12,18 +19,24 @@ public class Car {
         this.passengersOnboard = new ArrayList<Passenger>();
     }
     
+    /**Getter for max capacity of a car. */
     public int getCapacity(){ 
         return this.maxCapacity;
     }
 
+    /** Calculates the seats remaining in a car. */
     public int seatsRemaining(){ 
         return this.maxCapacity - this.passengersOnboard.size(); 
     }
 
+    /** Checks whether there is a seat available on a car or not. */
     private boolean seatAvailable(){
         return this.maxCapacity > this.passengersOnboard.size();
     }
 
+    /**Adds a passenger to a car.
+     * @param p The passenger
+     */
     //addPassenger - make sure enough seats
     public void addPassenger(Passenger p){
         if(seatAvailable()){
@@ -33,7 +46,10 @@ public class Car {
         }
     }
 
-    //removePassenger() - make sure they're actually on board first
+
+    /** Removes a passenger from a car if they are onboard.
+     * @param p The passenger
+     */
     public void removePassenger(Passenger p){
         if(this.passengersOnboard.contains(p)){
             this.passengersOnboard.remove(p);
@@ -42,7 +58,7 @@ public class Car {
         }
     }
 
-
+/** Prints the roster of passengers on a given car. */
    public void printManifest(){
         if(this.passengersOnboard.size() > 0){
             for(Passenger p : passengersOnboard){
@@ -55,6 +71,7 @@ public class Car {
         
    } 
 
+   /** Main function to test functions in class. */
     public static void main(String[] args) {
         Car myCar = new Car(15);
         Passenger pass1 = new Passenger("Logan");
